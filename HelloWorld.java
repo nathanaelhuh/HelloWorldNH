d090dd2d8978e12e52bdc9fd0e2336f6cebbe30d
matt <mtamkee@icloud.com>
2020-01-27 14:20:44 -0700

    Added user input to program

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,12 @@
+import java.util.Scanner;
+
 public class HelloWorld {
 	public static void main (String[] args)
 	{
-		System.out.println("Hello World");
+		System.out.println("Enter your name");
+		Scanner in = new Scanner(System.in);
+		String b;
+		b = in.nextLine();
+		System.out.println("Hello "+ b);
 	}
 }
